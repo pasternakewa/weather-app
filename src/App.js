79b30aadbe44571663c8ef4b7b4d6ba82a30add5
pasternakewa <ewapasternak.kontakt@gmail.com>
2020-12-06 01:54:12 +0100

    Add weather data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const getQueryUrl = (city) =>
+  `${process.env.REACT_APP_API_URL}?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [weatherData, setWeatherData] = useState();
+  const [city, setCity] = useState("Warszawa");
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(getQueryUrl(city))
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        console.log("e", e);
+        setError("fetch failed");
+      });
+  }, [city]);
+
+  if (loading) {
+    return <p>loading..</p>;
+  }
+
+  if (error) {
+    return <p>ERROR: {error}</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={city} onChange={(e) => setCity(e.target.value)} />
+      <p>Data:</p>
+      <p>{weatherData?.weather?.[0]?.description}</p>
     </div>
   );
 }
